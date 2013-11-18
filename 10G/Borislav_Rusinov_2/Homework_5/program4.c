2be55755fbeaf9ce20e12089ab117d2bcc60fb44
Admin <kingsize@ubuntu.(none)>
2013-11-18 18:20:39 +0200

    removed the old program4.c and updated the new one.

--- 10G/Borislav_Rusinov_2/Homework_5/program4.c
+++ 10G/Borislav_Rusinov_2/Homework_5/program4.c
@@ -1,16 +1,85 @@
 #include <stdio.h>
-int main ()
+
+int main()
 {
-	int m1,m2,m3,y,m;
-	scanf("%d",m1);
-	scanf("%d",m2);
-	scanf("%d",m3);
-	y=1*m1+2*m2+5*m3;
-	if(y>m)
+	int m1,m2,m3,m;
+	
+	scanf("%d", &m1);
+	scanf("%d", &m2);
+	scanf("%d", &m3);
+	scanf("%d", &m);
+	
+	int x = 0;
+	int y = 0;
+ 	int z = 0;
+
+	if ( (m1 + m2*2 + m3*5) < m)
 	{
-		
-	}else
-		printf("No");	
+		printf("No\n");
+		return 0;
+	}
+
+	if ( m - (m3 * 5) >= 0)
+		z = m3;
+	else
+	{
+		int i = 0;
+		for (i = m3; i != 0; i--)
+		{
+			if (m - (i * 5) >= 0)
+			{
+				z = i;
+				break;
+			}
+		}
+	}
+
+	m -= z*5;
+
+	if (m > 0)
+	{	
+		if ( m - (m2*2) >= 0)
+			y = m2;		
+		else
+		{
+			int i = 0;
+			for (i = m2; i != 0; i--)
+			{
+				if (m - (i * 2) >= 0)
+				{
+					y = i;
+					break;
+				}
+			}
+		}
+	}
+	m -= y*2;
+
+	if (m > 0)
+	{
+		if ( m - m1 >= 0)
+			x = m1;
+		else
+		{
+			int i = 0;
+			for (i = m1; i != 0; i--)
+			{
+				if (m - i >= 0)
+				{
+					x = i;
+					break;
+				}
+			}
+		}
+	}
+	m -= x;
+
+	if (m == 0)
+	{
+		printf("Yes %d %d %d\n", x, y, z);
+	}
+	else
+		printf("No\n");
 
 	return 0;
 }
